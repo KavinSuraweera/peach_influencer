a32bf26d644dc37196a20bd4902cdc6b5dd15b3a
KavinSuraweera <it20191474@my.sliit.lk>
2021-09-23 14:58:15 +0530

    sidie bar

--- app/src/main/java/com/adrenaline/peach/creator_dashboard.java
+++ app/src/main/java/com/adrenaline/peach/creator_dashboard.java
@@ -1,12 +1,15 @@
 package com.adrenaline.peach;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class creator_dashboard extends AppCompatActivity {
@@ -14,17 +17,30 @@ public class creator_dashboard extends AppCompatActivity {
     private Button btn_logout;
     private View decorView;
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creator_dashboard);
+
+
+        /*-----------------------------------------*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar );
+        /*-----------------------------------------*/
+        /*-----------------------------------------*/
+
 
         btn_logout = findViewById(R.id.btn_logout);
         decorView = getWindow().getDecorView();
         decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
             @Override
             public void onSystemUiVisibilityChange(int visibility) {
-                if(visibility == 0)
+                if (visibility == 0)
                     decorView.setSystemUiVisibility(hideSystemBars());
             }
         });
@@ -33,19 +49,20 @@ public class creator_dashboard extends AppCompatActivity {
     }
 
 
-
     public void logout1(View view) {
         FirebaseAuth.getInstance().signOut();//logout
-        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
         finish();
     }
 
-    private int hideSystemBars(){
+    private int hideSystemBars() {
         return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-    };
+    }
+
+    ;
 }
